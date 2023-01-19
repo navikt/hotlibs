e837de136761e35e1321323bddd1b96c5e7e6bd8
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2023-01-19 16:54:12 +0100

    Test fil

---
+++ src/test/kotlin/no/nav/hjelpemidler/http/slack/SlackTest.kt
@@ -0,0 +1,17 @@
+package no.nav.hjelpemidler.http.slack
+
+/* import kotlinx.coroutines.runBlocking
+import kotlin.test.Test
+
+class SlackTest {
+    private val slackWebHook = "<KREVES FOR TESTING MOT SLACK>"
+
+    @Test
+    fun `send testmelding til slack`() {
+        runBlocking {
+            val client = slack(DefaultSlackConfiguration(slackWebHook))
+            client.sendMessage("hm-http", slackIconEmoji(":grimacing:"), "#digihot-brukers-hjelpemiddelside-dev", "TEST, ignorer meg! :waving-from-afar-right:")
+            client.sendMessage("hm-http2", slackIconUrl("https://parade.com/.image/t_share/MTkwNTgxMTA1NjY0NDAyNTU3/funny-pictures.jpg"), "#digihot-brukers-hjelpemiddelside-dev", "TEST, ignorer meg! :waving-from-afar-right:")
+        }
+    }
+} */
