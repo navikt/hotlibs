76619058befefdab1aead777fd453f437d814da8
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2023-01-20 09:20:09 +0100

    Bruk kotlin.test.Ignore til å deaktivere slack teste i stede for å kommentere den ut.
    Co-authored-by: di0nys1us <erik.eiesland@nav.no>

--- src/test/kotlin/no/nav/hjelpemidler/http/slack/SlackTest.kt
+++ src/test/kotlin/no/nav/hjelpemidler/http/slack/SlackTest.kt
@@ -1,8 +1,10 @@
 package no.nav.hjelpemidler.http.slack
 
-/* import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
+import kotlin.test.Ignore
 
+@Ignore("Brukes kun til å teste Slack-integrasjon under utvikling")
 class SlackTest {
     private val slackWebHook = "<KREVES FOR TESTING MOT SLACK>"
 
@@ -14,4 +16,4 @@ class SlackTest {
             client.sendMessage("hm-http2", slackIconUrl("https://parade.com/.image/t_share/MTkwNTgxMTA1NjY0NDAyNTU3/funny-pictures.jpg"), "#digihot-brukers-hjelpemiddelside-dev", "TEST, ignorer meg! :waving-from-afar-right:")
         }
     }
-} */
+}
