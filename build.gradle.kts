84b95e390a9d9713a8f6f7de14ac1c2deb4bc468
Erik Eiesland <erik@eies.land>
2023-04-25 10:06:38 +0200

    ktor 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val coroutinesVersion = "1.6.4"
-val ktorVersion = "2.2.4"
+val ktorVersion = "2.3.0"
 val jacksonVersion = "2.14.2"
-val javaJwtVersion = "3.19.3" // følger ktor-server-auth-jwt
+val javaJwtVersion = "4.4.0" // følger ktor-server-auth-jwt
 val caffeineVersion = "3.1.5"
 val kotlinLoggingVersion = "3.0.5"
 val slf4jVersion = "2.0.3"
