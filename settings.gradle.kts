ea359b7af20be8100fcb90007f006c7883eddb60
Erik Eiesland <erik@eies.land>
2023-08-20 20:25:19 +0200

    hm-katalog 0.0.23

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nav.hjelpemidler:hm-katalog:0.0.22")
+            from("no.nav.hjelpemidler:hm-katalog:0.0.23")
         }
     }
 }
