04736cbd554a7580d31a9cb7f52ce66829431402
Erik Eiesland <erik@eies.land>
2023-09-12 14:57:43 +0200

    bruker Dispatchers.IO i withDatabaseContext

--- src/main/kotlin/no/nav/hjelpemidler/database/DatabaseContext.kt
+++ src/main/kotlin/no/nav/hjelpemidler/database/DatabaseContext.kt
@@ -1,15 +1,8 @@
 package no.nav.hjelpemidler.database
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.ExecutorCoroutineDispatcher
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-@OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
-val DatabaseContext: ExecutorCoroutineDispatcher =
-    newSingleThreadContext("DatabaseThread")
-
 suspend fun <T> withDatabaseContext(block: suspend CoroutineScope.() -> T): T =
-    withContext(context = DatabaseContext, block = block)
+    withContext(Dispatchers.IO, block)
