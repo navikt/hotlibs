0f89070aca13171c7b5bb3f87209adc6716a12b2
Erik Eiesland <di0nys1us@users.noreply.github.com>
2023-11-06 10:08:58 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nav.hjelpemidler:hm-katalog:0.0.35")
+            from("no.nav.hjelpemidler:hm-katalog:0.0.44")
         }
     }
 }
