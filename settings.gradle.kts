c23b78934fc05d6b0f3e6bdac054fab0fe531862
Erik Eiesland <erik.eiesland@nav.no>
2024-02-21 10:42:42 +0100

    oppdateringer i workflows, dependency-submission og bytter til gradle/actions/setup-gradle@v3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nav.hjelpemidler:hm-katalog:0.1.22")
+            from("no.nav.hjelpemidler:hm-katalog:0.1.23")
         }
     }
 }
