1c51a56851774b7c0291772c202086d840035ccd
Erik Eiesland <di0nys1us@users.noreply.github.com>
2024-03-21 11:27:00 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nav.hjelpemidler:hm-katalog:0.1.30")
+            from("no.nav.hjelpemidler:hm-katalog:0.1.33")
         }
     }
 }
