d6e355c4dfce734136637a6ddd085ba0ba94d084
Erik Eiesland <erik.eiesland@nav.no>
2024-09-16 15:57:39 +0200

    bruker VERSION_TAG i katalog

--- katalog/build.gradle.kts
+++ katalog/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "no.nav.hjelpemidler"
-version = System.getenv("GITHUB_REF_NAME") ?: "local"
+version = System.getenv("VERSION_TAG") ?: System.getenv("GITHUB_REF_NAME") ?: "local"
 
 repositories {
     gradlePluginPortal()
