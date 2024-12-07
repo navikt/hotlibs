ddd17488666e57139a012f766a286e6d053fea59
Erik Eiesland <erik.eiesland@nav.no>
2024-12-07 15:13:50 +0100

    legger til miljøvariabel INFLUX_URL

--- core/src/main/kotlin/no/nav/hjelpemidler/configuration/InfluxDBEnvironmentVariable.kt
+++ core/src/main/kotlin/no/nav/hjelpemidler/configuration/InfluxDBEnvironmentVariable.kt
@@ -9,17 +9,26 @@ package no.nav.hjelpemidler.configuration
  */
 object InfluxDBEnvironmentVariable {
     @External
-    val INFLUX_HOST by EnvironmentVariable
-
-    @External
-    val INFLUX_PORT by EnvironmentVariable
-
-    @External
-    val INFLUX_DATABASE_NAME by EnvironmentVariable
+    val INFLUX_URL by EnvironmentVariable
 
     @External
     val INFLUX_USER by EnvironmentVariable
 
     @External
     val INFLUX_PASSWORD by EnvironmentVariable
+
+    /**
+     * Bucket.
+     */
+    @External
+    val INFLUX_DATABASE_NAME by EnvironmentVariable
+
+    /**
+     * NB! Denne er definert med 'https://' i secret.
+     */
+    @External
+    val INFLUX_HOST by EnvironmentVariable
+
+    @External
+    val INFLUX_PORT by EnvironmentVariable
 }
