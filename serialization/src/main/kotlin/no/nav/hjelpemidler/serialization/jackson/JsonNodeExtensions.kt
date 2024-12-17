53ac7ab867bc125c9c5e524dead6fcb1e387d665
Erik Eiesland <erik.eiesland@nav.no>
2024-12-17 17:49:42 +0100

    extensions for å gjøre om JsonNode til Enum

--- serialization/src/main/kotlin/no/nav/hjelpemidler/serialization/jackson/JsonNodeExtensions.kt
+++ serialization/src/main/kotlin/no/nav/hjelpemidler/serialization/jackson/JsonNodeExtensions.kt
@@ -7,5 +7,8 @@ import java.util.UUID
 fun JsonNode.uuidValue(): UUID = UUID.fromString(textValue())
 fun JsonNode.uuidValueOrNull(): UUID? = textValue()?.let(UUID::fromString)
 
+inline fun <reified E : Enum<E>> JsonNode.enumValue(): E = enumValueOf<E>(textValue())
+inline fun <reified E : Enum<E>> JsonNode.enumValueOrNull(): E? = textValue()?.let { enumValueOf<E>(it) }
+
 fun JsonNode.fødselsnummerValue(): Fødselsnummer = Fødselsnummer(textValue())
 fun JsonNode.fødselsnummerValueOrNull(): Fødselsnummer? = textValue()?.let(::Fødselsnummer)
