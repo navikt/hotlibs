f606049a0af610f3eb5368c395091f21c5a80602
Erik Eiesland <erik.eiesland@nav.no>
2024-12-20 11:47:22 +0100

    extensions for å gjøre om JsonNode til T eller T?

--- serialization/src/main/kotlin/no/nav/hjelpemidler/serialization/jackson/JsonNodeExtensions.kt
+++ serialization/src/main/kotlin/no/nav/hjelpemidler/serialization/jackson/JsonNodeExtensions.kt
@@ -1,6 +1,7 @@
 package no.nav.hjelpemidler.serialization.jackson
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.treeToValue
 import no.nav.hjelpemidler.domain.person.Fødselsnummer
 import java.util.UUID
 
@@ -12,3 +13,6 @@ inline fun <reified E : Enum<E>> JsonNode.enumValueOrNull(): E? = textValue()?.l
 
 fun JsonNode.fødselsnummerValue(): Fødselsnummer = Fødselsnummer(textValue())
 fun JsonNode.fødselsnummerValueOrNull(): Fødselsnummer? = textValue()?.let(::Fødselsnummer)
+
+inline fun <reified T : Any> JsonNode.value(): T = jsonMapper.treeToValue(this)
+inline fun <reified T : Any> JsonNode?.valueOrNull(): T? = this?.let(jsonMapper::treeToValue)
