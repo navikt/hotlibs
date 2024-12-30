40f92fcaf2a21818c78aa7f80997d77ddf5fe0b2
Erik Eiesland <erik.eiesland@nav.no>
2024-12-30 15:34:30 +0100

    legger til protobuf-bom i platform grunnet sårbarhet

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     api(platform(libs.kotlinx.coroutines.bom))
     api(platform(libs.kotlinx.serialization.bom))
     api(platform(libs.ktor.bom))
+    api(platform(libs.protobuf.bom))
     api(platform(libs.slf4j.bom))
 
     constraints {
