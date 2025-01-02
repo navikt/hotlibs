b3917731b0faaa3310056e97050e32de88d646ea
Erik Eiesland <erik.eiesland@nav.no>
2025-01-02 10:36:26 +0100

    metode for å lage role hvis den ikke finnes fra før

--- database/src/postgresql/kotlin/no/nav/hjelpemidler/database/FlywayExtensions.kt
+++ database/src/postgresql/kotlin/no/nav/hjelpemidler/database/FlywayExtensions.kt
@@ -5,3 +5,24 @@ import org.intellij.lang.annotations.Language
 
 fun FluentConfiguration.initSql(@Language("SQL") vararg initSql: String): FluentConfiguration =
     initSql(initSql.joinToString(";"))
+
+
+fun FluentConfiguration.createRole(role: String) {
+    initSql(
+        initSql ?: "",
+        """
+            DO
+            ${'$'}do${'$'}
+                BEGIN
+                    IF EXISTS (SELECT
+                               FROM pg_catalog.pg_roles
+                               WHERE rolname = '$role') THEN
+                        RAISE NOTICE '"$role" finnes allerede.';
+                    ELSE
+                        CREATE ROLE $role;
+                    END IF;
+                END
+            ${'$'}do${'$'};
+        """.trimIndent(),
+    )
+}
