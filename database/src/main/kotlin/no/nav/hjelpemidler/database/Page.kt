8395af3ba22b381cafb7ec7305519f5c1b1061cb
Erik Eiesland <erik.eiesland@nav.no>
2025-01-27 09:22:14 +0100

    endrer Page til Iterable slik at Jackson ikke serialiserer som List

--- database/src/main/kotlin/no/nav/hjelpemidler/database/Page.kt
+++ database/src/main/kotlin/no/nav/hjelpemidler/database/Page.kt
@@ -3,7 +3,7 @@ package no.nav.hjelpemidler.database
 import com.fasterxml.jackson.annotation.JsonIgnore
 import kotlin.math.ceil
 
-interface Page<out T> : List<T> {
+interface Page<out T> : Iterable<T> {
     val content: List<T>
     val totalElements: Long
     val pageNumber: Int
@@ -16,7 +16,7 @@ internal data class DefaultPage<out T>(
     override val totalElements: Long,
     @JsonIgnore
     val pageRequest: PageRequest,
-) : Page<T>, List<T> by content {
+) : Page<T>, Iterable<T> by content {
     override val pageNumber: Int get() = pageRequest.pageNumber
     override val pageSize: Int get() = pageRequest.pageSize
     override val totalPages: Int get() = ceil(totalElements.toDouble() / pageSize).toInt()
