b78a8ad42c13abbf4472b5f8e694c635c27700ed
Erik Eiesland <erik.eiesland@nav.no>
2025-02-26 10:27:52 +0100

    fiks for CVE-2024-57699

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -6,6 +6,10 @@ dependencies {
     api(libs.kotlinx.coroutines.test)
     api(libs.kotest.assertions.core)
     api(libs.kotest.assertions.json)
+    constraints {
+        // CVE-2024-57699
+        implementation("net.minidev:json-smart:2.5.2")
+    }
     api(libs.mockk) {
         exclude("junit", "junit")
     }
