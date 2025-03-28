c9715f93a57eb75a5a0e479756eee1d9763c28e7
Erik Eiesland <erik.eiesland@nav.no>
2025-03-28 14:06:09 +0100

    nye versjoner, bla. ktor 3.1.2, confluent 7.9.0, graphql 8.4.0
    oppdateringen er gjort med versjon 1.0.0 av nl.littlerobots.version-catalog-update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,10 @@
 plugins {
-    alias(libs.plugins.versions)
     alias(libs.plugins.version.catalog.update)
 }
 
 versionCatalogUpdate {
     sortByKey = true
-    pin {
-        versions.addAll(
-            "kotest",
-            "kotlin",
-            "slf4j",
-            "tbdLibs",
-        )
-    }
     keep {
         keepUnusedVersions = true
-        keepUnusedPlugins = true
-        keepUnusedLibraries = true
     }
 }
