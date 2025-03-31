e5ba088120d53568a52a6f9be79b3e5479a7b7bf
Erik Eiesland <erik.eiesland@nav.no>
2025-03-31 12:08:10 +0200

    endrer rekkefølge på interface og funksjoner

--- database/src/main/kotlin/no/nav/hjelpemidler/database/sql/Sql.kt
+++ database/src/main/kotlin/no/nav/hjelpemidler/database/sql/Sql.kt
@@ -3,9 +3,9 @@ package no.nav.hjelpemidler.database.sql
 import org.intellij.lang.annotations.Language
 
 @JvmInline
-value class Sql(@Language("SQL") private val value: String) : CharSequence by value, Comparable<Sql> {
+value class Sql(@Language("SQL") private val value: String) : Comparable<Sql>, CharSequence by value {
+    fun replace(oldValue: String, newValue: String): Sql = Sql(value.replace(oldValue, newValue))
+
     override fun compareTo(other: Sql): Int = value.compareTo(other.value)
     override fun toString(): String = value
-
-    fun replace(oldValue: String, newValue: String): Sql = Sql(value.replace(oldValue, newValue))
 }
