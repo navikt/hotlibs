8cf42647591a5925b7e9bb3b97c06eb97b4a2e33
Erik Eiesland <erik.eiesland@nav.no>
2025-04-09 13:19:08 +0200

    endrer rekkefølge på argumenter i autoriser() slik at det matcher tilsvarende funksjoner
    ny versjon av koin og coroutines

--- nare/src/main/kotlin/no/nav/hjelpemidler/nare/policy/Autoriser.kt
+++ nare/src/main/kotlin/no/nav/hjelpemidler/nare/policy/Autoriser.kt
@@ -1,4 +1,8 @@
 package no.nav.hjelpemidler.nare.policy
 
-inline fun <T : Any, R> autoriser(context: T, policy: Policy<T>, block: (Policyevaluering) -> R): R =
-    policy.evaluer(context).let { block.invoke(it) }
+@JvmName("autoriserExt")
+inline fun <T : Any, R> Policy<T>.autoriser(context: T, block: (Policyevaluering) -> R): R =
+    evaluer(context).let(block)
+
+inline fun <T : Any, R> autoriser(policy: Policy<T>, context: T, block: (Policyevaluering) -> R): R =
+    policy.autoriser(context, block)
