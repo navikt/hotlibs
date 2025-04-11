cbf64d6036f9ff13f0ea72910f1dc49a9e47c57f
Erik Eiesland <erik.eiesland@nav.no>
2025-04-11 11:00:49 +0200

    fjerner ubrukt klasse

--- nare/src/main/kotlin/no/nav/hjelpemidler/nare/evaluering/Evaluering.kt
+++ nare/src/main/kotlin/no/nav/hjelpemidler/nare/evaluering/Evaluering.kt
@@ -7,29 +7,3 @@ interface Evaluering<T : LogiskOperand<T>> {
     val begrunnelse: String
     val operator: Operator
 }
-
-data class DefaultEvaluering<T : LogiskOperand<T>>(
-    override val resultat: T,
-    override val begrunnelse: String,
-    override val operator: Operator = Operator.INGEN,
-) : Evaluering<T>, LogiskOperand<DefaultEvaluering<T>> {
-    override fun og(annen: DefaultEvaluering<T>): DefaultEvaluering<T> = DefaultEvaluering(
-        resultat = resultat og annen.resultat,
-        begrunnelse = "($begrunnelse OG ${annen.begrunnelse})",
-        operator = Operator.OG,
-    )
-
-    override fun eller(annen: DefaultEvaluering<T>): DefaultEvaluering<T> = DefaultEvaluering(
-        resultat = resultat eller annen.resultat,
-        begrunnelse = "($begrunnelse ELLER ${annen.begrunnelse})",
-        operator = Operator.ELLER,
-    )
-
-    override fun ikke(): DefaultEvaluering<T> = DefaultEvaluering(
-        resultat = resultat.ikke(),
-        begrunnelse = "(IKKE $begrunnelse)",
-        operator = Operator.IKKE,
-    )
-
-    override fun toString(): String = "$resultat(begrunnelse: '$begrunnelse')"
-}
