24454d38c1df03e063034ae7f7e2e6917041ac0f
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2025-05-27 13:05:45 +0200

    Legg til logextensions for TeamLogs

---
+++ logging/src/main/kotlin/no/nav/hjelpemidler/logging/TeamLogsExtensions.kt
@@ -0,0 +1,12 @@
+package no.nav.hjelpemidler.logging
+
+import io.github.oshai.kotlinlogging.KLogger
+import io.github.oshai.kotlinlogging.KMarkerFactory
+import io.github.oshai.kotlinlogging.Marker
+
+val teamLogsMarker = KMarkerFactory.getMarker("TEAM_LOGS")
+fun KLogger.teamTrace(throwable: Throwable? = null, message: () -> Any?) = trace(throwable, teamLogsMarker, message)
+fun KLogger.teamDebug(throwable: Throwable? = null, message: () -> Any?) = debug(throwable, teamLogsMarker, message)
+fun KLogger.teamInfo(throwable: Throwable? = null, message: () -> Any?) = info(throwable, teamLogsMarker, message)
+fun KLogger.teamWarn(throwable: Throwable? = null, message: () -> Any?) = warn(throwable, teamLogsMarker, message)
+fun KLogger.teamError(throwable: Throwable? = null, message: () -> Any?) = error(throwable, teamLogsMarker, message)
