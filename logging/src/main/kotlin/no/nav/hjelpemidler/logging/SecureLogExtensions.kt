cde762a2dc721d63f8e29ad34dee1bfb38b89f11
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2025-05-28 10:54:07 +0200

    Marker secureLog som deprecated

--- logging/src/main/kotlin/no/nav/hjelpemidler/logging/SecureLogExtensions.kt
+++ logging/src/main/kotlin/no/nav/hjelpemidler/logging/SecureLogExtensions.kt
@@ -5,8 +5,13 @@ import io.github.oshai.kotlinlogging.KMarkerFactory
 import io.github.oshai.kotlinlogging.Marker
 
 val secureLogMarker: Marker = KMarkerFactory.getMarker("secureLog")
+@Deprecated("Bytt til Team Logs. Se docs: https://docs.nais.io/observability/logging/how-to/team-logs/")
 fun KLogger.secureTrace(throwable: Throwable? = null, message: () -> Any?) = trace(throwable, secureLogMarker, message)
+@Deprecated("Bytt til Team Logs. Se docs: https://docs.nais.io/observability/logging/how-to/team-logs/")
 fun KLogger.secureDebug(throwable: Throwable? = null, message: () -> Any?) = debug(throwable, secureLogMarker, message)
+@Deprecated("Bytt til Team Logs. Se docs: https://docs.nais.io/observability/logging/how-to/team-logs/")
 fun KLogger.secureInfo(throwable: Throwable? = null, message: () -> Any?) = info(throwable, secureLogMarker, message)
+@Deprecated("Bytt til Team Logs. Se docs: https://docs.nais.io/observability/logging/how-to/team-logs/")
 fun KLogger.secureWarn(throwable: Throwable? = null, message: () -> Any?) = warn(throwable, secureLogMarker, message)
+@Deprecated("Bytt til Team Logs. Se docs: https://docs.nais.io/observability/logging/how-to/team-logs/")
 fun KLogger.secureError(throwable: Throwable? = null, message: () -> Any?) = error(throwable, secureLogMarker, message)
