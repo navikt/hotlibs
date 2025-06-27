a7f25e5c27e694f7f201c4d6fae8d28b47cd78dd
Erik Eiesland <erik.eiesland@nav.no>
2025-06-27 12:37:16 +0200

    fjerner limitedParallelism, det er ikke bare JDBC-kall som gjøres i transaction

--- database/src/main/kotlin/no/nav/hjelpemidler/database/DatabaseContext.kt
+++ database/src/main/kotlin/no/nav/hjelpemidler/database/DatabaseContext.kt
@@ -1,12 +1,8 @@
 package no.nav.hjelpemidler.database
 
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-internal val DatabaseDispatcher: CoroutineDispatcher =
-    Dispatchers.IO.limitedParallelism(parallelism = HIKARI_MAXIMUM_POOL_SIZE, name = "DatabaseDispatcher")
-
 suspend fun <T> withDatabaseContext(block: suspend CoroutineScope.() -> T): T =
-    withContext(DatabaseDispatcher, block)
+    withContext(Dispatchers.IO, block)
