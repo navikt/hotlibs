d21ba124f6c91a6ae002ee4f66e39ce99b05bbb0
Erik Eiesland <erik.eiesland@nav.no>
2025-07-01 16:01:54 +0200

    health checks for hotlibs/streams

---
+++ streams/src/main/kotlin/no/nav/hjelpemidler/streams/KafkaStreamsHealth.kt
@@ -0,0 +1,26 @@
+package no.nav.hjelpemidler.streams
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+import org.apache.kafka.streams.KafkaStreams
+
+fun Route.health(kafkaStreams: KafkaStreams) {
+    get("/isalive") {
+        val state = kafkaStreams.state()
+        if (state == KafkaStreams.State.RUNNING || state == KafkaStreams.State.REBALANCING) {
+            call.respond(HttpStatusCode.OK)
+        } else {
+            call.respond(HttpStatusCode.ServiceUnavailable, "KafkaStreams state: $state")
+        }
+    }
+    get("/isready") {
+        val state = kafkaStreams.state()
+        if (state == KafkaStreams.State.RUNNING) {
+            call.respond(HttpStatusCode.OK)
+        } else {
+            call.respond(HttpStatusCode.ServiceUnavailable, "KafkaStreams state: $state")
+        }
+    }
+}
