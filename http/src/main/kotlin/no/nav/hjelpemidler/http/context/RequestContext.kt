185399e2480187c53cfe79db50265e001cccfd7e
Erik Eiesland <erik.eiesland@nav.no>
2025-08-11 17:00:32 +0200

    lenke i RequestContext

--- http/src/main/kotlin/no/nav/hjelpemidler/http/context/RequestContext.kt
+++ http/src/main/kotlin/no/nav/hjelpemidler/http/context/RequestContext.kt
@@ -5,6 +5,9 @@ import no.nav.hjelpemidler.security.Principal
 import kotlin.coroutines.AbstractCoroutineContextElement
 import kotlin.coroutines.CoroutineContext
 
+/**
+ * @see <a href="https://blog.tpersson.io/2018/04/22/emulating-request-scoped-objects-with-kotlin-coroutines/">Mer informasjon.</a>
+ */
 class RequestContext(val principal: Principal<*>) : AbstractCoroutineContextElement(RequestContext) {
     companion object Key : CoroutineContext.Key<RequestContext>
 }
