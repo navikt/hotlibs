34d9abedc122b038d74ae3cc7bae54c89ce49ba1
Erik Eiesland <erik.eiesland@nav.no>
2025-09-30 07:52:24 +0200

    ValueType er Serializable

--- core/src/main/kotlin/no/nav/hjelpemidler/domain/ValueType.kt
+++ core/src/main/kotlin/no/nav/hjelpemidler/domain/ValueType.kt
@@ -1,8 +1,10 @@
 package no.nav.hjelpemidler.domain
 
+import java.io.Serializable
+
 /**
  * Wrapper for en enkelt verdi.
  */
-interface ValueType<T : Comparable<T>> {
+interface ValueType<T : Comparable<T>> : Serializable {
     val value: T
 }
