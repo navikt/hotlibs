b4f8c97fb78530187b85fa1f22c5ba1ba2d4991d
Erik Eiesland <erik.eiesland@nav.no>
2026-01-09 08:31:53 +0100

    fjerner alias for TilknyttetEnhet

--- core/src/main/kotlin/no/nav/hjelpemidler/domain/geografi/TilknyttetEnhet.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.hjelpemidler.domain.geografi
-
-/**
- * NB! Denne skal fjernes.
- * Bruk [no.nav.hjelpemidler.domain.enhet.TilknyttetEnhet] direkte.
- */
-typealias TilknyttetEnhet = no.nav.hjelpemidler.domain.enhet.TilknyttetEnhet
