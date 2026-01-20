95ff06a1d2a5b20b3b8d0d8565bbe23113999b9c
Erik Eiesland <erik.eiesland@nav.no>
2026-01-20 10:42:27 +0100

    Fødselsdato.alderIDag extension

--- core/src/main/kotlin/no/nav/hjelpemidler/domain/person/Fødselsdato.kt
+++ core/src/main/kotlin/no/nav/hjelpemidler/domain/person/Fødselsdato.kt
@@ -2,11 +2,13 @@ package no.nav.hjelpemidler.domain.person
 
 import no.nav.hjelpemidler.time.iDag
 import java.time.LocalDate
-import java.time.Period
+import java.time.temporal.ChronoUnit
 
 typealias Fødselsdato = LocalDate
 
-fun Fødselsdato.alder(dag: LocalDate = iDag()): Int = Period.between(this, dag).years
+fun Fødselsdato.alderPer(dag: LocalDate = iDag()): Int = ChronoUnit.YEARS.between(this, dag).toInt()
+
+val Fødselsdato.alderIDag: Int get() = alderPer(iDag())
 
 infix fun Int.år(dag: LocalDate): Fødselsdato = dag.minusYears(toLong())
 
--- core/src/main/kotlin/no/nav/hjelpemidler/time/Time.kt
+++ core/src/main/kotlin/no/nav/hjelpemidler/time/Time.kt
@@ -3,70 +3,39 @@ package no.nav.hjelpemidler.time
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.OffsetDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.util.Date
 import java.util.TimeZone
 import kotlin.time.Duration
 
-val TIME_ZONE_EUROPE_OSLO: TimeZone =
-    TimeZone.getTimeZone("Europe/Oslo")
+val TIME_ZONE_EUROPE_OSLO: TimeZone = TimeZone.getTimeZone("Europe/Oslo")
+val ZONE_ID_EUROPE_OSLO: ZoneId = TIME_ZONE_EUROPE_OSLO.toZoneId()
 
-val ZONE_ID_EUROPE_OSLO: ZoneId =
-    TIME_ZONE_EUROPE_OSLO.toZoneId()
+fun nå(): Instant = Instant.now()
+fun iDag(): LocalDate = LocalDate.now()
 
-fun nå(): Instant =
-    Instant.now()
-
-fun nåtz(): ZonedDateTime =
-    ZonedDateTime.now(ZONE_ID_EUROPE_OSLO)
-
-fun iDag(): LocalDate =
-    LocalDate.now()
+fun nowWithOffset(): OffsetDateTime = OffsetDateTime.now(ZONE_ID_EUROPE_OSLO)
+fun nowWithTimeZone(): ZonedDateTime = ZonedDateTime.now(ZONE_ID_EUROPE_OSLO)
 
 // Date
-
-fun Date.toLocalDate(): LocalDate =
-    toInstant().toLocalDate()
-
-fun Date.toLocalDateTime(): LocalDateTime =
-    toInstant().toLocalDateTime()
+fun Date.toLocalDate(): LocalDate = toInstant().toLocalDate()
+fun Date.toLocalDateTime(): LocalDateTime = toInstant().toLocalDateTime()
 
 // LocalDate
-
-fun LocalDate.toInstant(): Instant =
-    atStartOfDay().atZone(ZONE_ID_EUROPE_OSLO).toInstant()
-
-fun LocalDate.toDate(): Date =
-    toInstant().toDate()
-
-infix fun LocalDate.erISammeÅrSom(other: LocalDate): Boolean =
-    year == other.year
+fun LocalDate.toInstant(): Instant = atStartOfDay().atZone(ZONE_ID_EUROPE_OSLO).toInstant()
+fun LocalDate.toDate(): Date = toInstant().toDate()
+infix fun LocalDate.erISammeÅrSom(other: LocalDate): Boolean = year == other.year
 
 // LocalDateTime
-
-fun LocalDateTime.toInstant(): Instant =
-    atZone(ZONE_ID_EUROPE_OSLO).toInstant()
-
-fun LocalDateTime.toDate(): Date =
-    toInstant().toDate()
+fun LocalDateTime.toInstant(): Instant = atZone(ZONE_ID_EUROPE_OSLO).toInstant()
+fun LocalDateTime.toDate(): Date = toInstant().toDate()
 
 // Instant
-
-fun Instant.toLocalDate(): LocalDate =
-    ZonedDateTime.ofInstant(this, ZONE_ID_EUROPE_OSLO).toLocalDate()
-
-fun Instant.toLocalDateTime(): LocalDateTime =
-    ZonedDateTime.ofInstant(this, ZONE_ID_EUROPE_OSLO).toLocalDateTime()
-
-fun Instant.toZonedDateTime(): ZonedDateTime =
-    atZone(ZONE_ID_EUROPE_OSLO)
-
-fun Instant.toDate(): Date =
-    Date.from(this)
-
-operator fun Instant.minus(duration: Duration): Instant =
-    minusNanos(duration.inWholeNanoseconds)
-
-operator fun Instant.plus(duration: Duration): Instant =
-    plusNanos(duration.inWholeNanoseconds)
+fun Instant.toLocalDate(): LocalDate = ZonedDateTime.ofInstant(this, ZONE_ID_EUROPE_OSLO).toLocalDate()
+fun Instant.toLocalDateTime(): LocalDateTime = ZonedDateTime.ofInstant(this, ZONE_ID_EUROPE_OSLO).toLocalDateTime()
+fun Instant.toZonedDateTime(): ZonedDateTime = atZone(ZONE_ID_EUROPE_OSLO)
+fun Instant.toDate(): Date = Date.from(this)
+operator fun Instant.minus(duration: Duration): Instant = minusNanos(duration.inWholeNanoseconds)
+operator fun Instant.plus(duration: Duration): Instant = plusNanos(duration.inWholeNanoseconds)
