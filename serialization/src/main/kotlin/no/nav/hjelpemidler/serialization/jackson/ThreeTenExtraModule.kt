c31c4415f392ecf69b8552762897f45acb3f9387
Erik Eiesland <erik.eiesland@nav.no>
2026-01-29 11:48:50 +0100

    forenklinger i ThreeTenExtraModule

--- serialization/src/main/kotlin/no/nav/hjelpemidler/serialization/jackson/ThreeTenExtraModule.kt
+++ serialization/src/main/kotlin/no/nav/hjelpemidler/serialization/jackson/ThreeTenExtraModule.kt
@@ -25,60 +25,59 @@ import kotlin.reflect.KClass
 internal class ThreeTenExtraModule : SimpleModule() {
     init {
         setOf(
-            Days::class,
-            HourMinute::class,
-            Hours::class,
-            Interval::class,
-            LocalDateRange::class,
-            Minutes::class,
-            Months::class,
+            // Temporal
             OffsetDate::class,
-            PeriodDuration::class,
-            Seconds::class,
-            Weeks::class,
-            YearHalf::class,
-            YearQuarter::class,
+            HourMinute::class,
             YearWeek::class,
-            Years::class,
-        ).forEach { type -> addSerializer(type.java, ToStringSerializer()) }
+            YearQuarter::class,
+            YearHalf::class,
 
-        addDeserializer(Days::class, DaysDeserializer())
-        addDeserializer(HourMinute::class, HourMinuteDeserializer())
-        addDeserializer(Hours::class, HoursDeserializer())
-        addDeserializer(Interval::class, IntervalDeserializer())
-        addDeserializer(LocalDateRange::class, LocalDateRangeDeserializer())
-        addDeserializer(Minutes::class, MinutesDeserializer())
-        addDeserializer(Months::class, MonthsDeserializer())
-        addDeserializer(OffsetDate::class, OffsetDateDeserializer())
-        addDeserializer(PeriodDuration::class, PeriodDurationDeserializer())
-        addDeserializer(Seconds::class, SecondsDeserializer())
-        addDeserializer(Weeks::class, WeeksDeserializer())
-        addDeserializer(YearHalf::class, YearHalfDeserializer())
-        addDeserializer(YearQuarter::class, YearQuarterDeserializer())
-        addDeserializer(YearWeek::class, YearWeekDeserializer())
-        addDeserializer(Years::class, YearsDeserializer())
+            // TemporalAmount
+            Seconds::class,
+            Minutes::class,
+            Hours::class,
+            Days::class,
+            Weeks::class,
+            Months::class,
+            Years::class,
+            PeriodDuration::class,
+
+            // Range
+            LocalDateRange::class,
+
+            // Interval
+            Interval::class,
+        ).forEach { type -> addSerializer(type.java, ToStringSerializer.instance) }
+
+        // Temporal
+        addDeserializer(OffsetDate::class, OffsetDate::parse)
+        addDeserializer(HourMinute::class, HourMinute::parse)
+        addDeserializer(YearWeek::class, YearWeek::parse)
+        addDeserializer(YearQuarter::class, YearQuarter::parse)
+        addDeserializer(YearHalf::class, YearHalf::parse)
+
+        // TemporalAmount
+        addDeserializer(Seconds::class, Seconds::parse)
+        addDeserializer(Minutes::class, Minutes::parse)
+        addDeserializer(Hours::class, Hours::parse)
+        addDeserializer(Days::class, Days::parse)
+        addDeserializer(Weeks::class, Weeks::parse)
+        addDeserializer(Months::class, Months::parse)
+        addDeserializer(Years::class, Years::parse)
+        addDeserializer(PeriodDuration::class, PeriodDuration::parse)
+
+        // Range
+        addDeserializer(LocalDateRange::class, LocalDateRange::parse)
+
+        // Interval
+        addDeserializer(Interval::class, Interval::parse)
     }
 
-    class DaysDeserializer : Deserializer<Days>(Days::class, Days::parse)
-    class HourMinuteDeserializer : Deserializer<HourMinute>(HourMinute::class, HourMinute::parse)
-    class HoursDeserializer : Deserializer<Hours>(Hours::class, Hours::parse)
-    class IntervalDeserializer : Deserializer<Interval>(Interval::class, Interval::parse)
-    class LocalDateRangeDeserializer : Deserializer<LocalDateRange>(LocalDateRange::class, LocalDateRange::parse)
-    class MinutesDeserializer : Deserializer<Minutes>(Minutes::class, Minutes::parse)
-    class MonthsDeserializer : Deserializer<Months>(Months::class, Months::parse)
-    class OffsetDateDeserializer : Deserializer<OffsetDate>(OffsetDate::class, OffsetDate::parse)
-    class PeriodDurationDeserializer : Deserializer<PeriodDuration>(PeriodDuration::class, PeriodDuration::parse)
-    class SecondsDeserializer : Deserializer<Seconds>(Seconds::class, Seconds::parse)
-    class WeeksDeserializer : Deserializer<Weeks>(Weeks::class, Weeks::parse)
-    class YearHalfDeserializer : Deserializer<YearHalf>(YearHalf::class, YearHalf::parse)
-    class YearQuarterDeserializer : Deserializer<YearQuarter>(YearQuarter::class, YearQuarter::parse)
-    class YearWeekDeserializer : Deserializer<YearWeek>(YearWeek::class, YearWeek::parse)
-    class YearsDeserializer : Deserializer<Years>(Years::class, Years::parse)
+    private fun <T : Any> addDeserializer(type: KClass<T>, creator: (rawValue: String) -> T) =
+        addDeserializer(type, Deserializer(type, creator))
 
-    abstract class Deserializer<T : Any>(
-        type: KClass<T>,
-        private val creator: (rawValue: String) -> T,
-    ) : FromStringDeserializer<T>(type.java) {
+    class Deserializer<T : Any>(type: KClass<T>, private val creator: (rawValue: String) -> T) :
+        FromStringDeserializer<T>(type.java) {
         override fun _deserialize(value: String, context: DeserializationContext): T = creator(value)
     }
 }
