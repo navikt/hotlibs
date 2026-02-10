07c6b66dacee225484e03a07b8639ff8808cb598
Erik Eiesland <erik.eiesland@nav.no>
2026-02-10 17:10:37 +0100

    exclude logstash-logback-encoder fra rapids-and-rivers
    logstash-logback-encoder 9.0 tar inn Jackson 3

--- rapids-and-rivers/build.gradle.kts
+++ rapids-and-rivers/build.gradle.kts
@@ -6,7 +6,10 @@ dependencies {
     api(project(":kafka"))
     api(project(":logging"))
     api(project(":serialization"))
-    api(libs.rapidsAndRivers)
+    api(libs.rapidsAndRivers) {
+        // rapids-and-rivers har endret til 9.0 som tar inn Jackson 3.0
+        exclude(module = "logstash-logback-encoder")
+    }
 
     testImplementation(libs.tbdLibs.rapidsAndRivers.test)
 }
