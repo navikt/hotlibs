45bc4645aa32859b76b7c1bb116365c0ff48abe9
Erik Eiesland <erik.eiesland@nav.no>
2026-02-16 15:34:54 +0100

    nye versjoner, bla. ny rapids-and-rivers og postgresql-driver

---
+++ database/src/main/kotlin/no/nav/hjelpemidler/database/annotation/JdbcResult.kt
@@ -0,0 +1,11 @@
+package no.nav.hjelpemidler.database.annotation
+
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@JacksonAnnotationsInside
+@JsonNaming(DatabasePropertyNamingStrategy::class)
+annotation class JdbcResult
