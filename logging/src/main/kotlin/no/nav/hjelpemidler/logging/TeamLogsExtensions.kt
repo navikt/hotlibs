42ad0c1d76dbf290dc00cf2a4d00838dbbd70c60
Erik Eiesland <erik.eiesland@nav.no>
2026-02-19 08:05:58 +0100

    nye versjoner, bla. kotlin-logging 8 med API-endring

--- logging/src/main/kotlin/no/nav/hjelpemidler/logging/TeamLogsExtensions.kt
+++ logging/src/main/kotlin/no/nav/hjelpemidler/logging/TeamLogsExtensions.kt
@@ -6,8 +6,8 @@ import io.github.oshai.kotlinlogging.Marker
 
 internal val teamLogsMarker: Marker = KMarkerFactory.getMarker("TEAM_LOGS")
 
-fun KLogger.teamTrace(throwable: Throwable? = null, message: () -> Any?) = trace(throwable, teamLogsMarker, message)
-fun KLogger.teamDebug(throwable: Throwable? = null, message: () -> Any?) = debug(throwable, teamLogsMarker, message)
-fun KLogger.teamInfo(throwable: Throwable? = null, message: () -> Any?) = info(throwable, teamLogsMarker, message)
-fun KLogger.teamWarn(throwable: Throwable? = null, message: () -> Any?) = warn(throwable, teamLogsMarker, message)
-fun KLogger.teamError(throwable: Throwable? = null, message: () -> Any?) = error(throwable, teamLogsMarker, message)
+fun KLogger.teamTrace(throwable: Throwable? = null, message: () -> Any?) = trace(teamLogsMarker, throwable, message)
+fun KLogger.teamDebug(throwable: Throwable? = null, message: () -> Any?) = debug(teamLogsMarker, throwable, message)
+fun KLogger.teamInfo(throwable: Throwable? = null, message: () -> Any?) = info(teamLogsMarker, throwable, message)
+fun KLogger.teamWarn(throwable: Throwable? = null, message: () -> Any?) = warn(teamLogsMarker, throwable, message)
+fun KLogger.teamError(throwable: Throwable? = null, message: () -> Any?) = error(teamLogsMarker, throwable, message)
