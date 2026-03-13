a5ac398a8ae1e2b6922438f88dfeeebed6667c58
Erik Eiesland <erik.eiesland@nav.no>
2026-03-13 16:39:38 +0100

    map/flatMap i Maybe

--- core/src/main/kotlin/no/nav/hjelpemidler/domain/Maybe.kt
+++ core/src/main/kotlin/no/nav/hjelpemidler/domain/Maybe.kt
@@ -1,7 +1,18 @@
 package no.nav.hjelpemidler.domain
 
 sealed interface Maybe<out T> {
-    val orNull: T? get() = (this as? Present)?.value
+    val orNull: T?
+        get() = when (this) {
+            Absent -> null
+            is Present -> value
+        }
+
+    fun <R> map(transform: (T?) -> R?): Maybe<R> = flatMap { Present(transform(it)) }
+
+    fun <R> flatMap(transform: (T?) -> Maybe<R>): Maybe<R> = when (this) {
+        Absent -> Absent
+        is Present -> transform(value)
+    }
 
     data object Absent : Maybe<Nothing>
     data class Present<out T>(val value: T?) : Maybe<T>
