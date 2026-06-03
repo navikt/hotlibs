1edd97e3ed49ee4f7a10856bed52e4539488e837
Erik Eiesland <erik.eiesland@nav.no>
2026-06-03 11:05:55 +0200

    fix: fjerner unødvendig inline/reified
    fjerner også context switch som ikke trengs

--- kafka/src/main/kotlin/no/nav/hjelpemidler/kafka/ProducerExtensions.kt
+++ kafka/src/main/kotlin/no/nav/hjelpemidler/kafka/ProducerExtensions.kt
@@ -1,8 +1,7 @@
 package no.nav.hjelpemidler.kafka
 
-import kotlinx.coroutines.Dispatchers
+import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlinx.coroutines.withContext
 import org.apache.kafka.clients.producer.Callback
 import org.apache.kafka.clients.producer.Producer
 import org.apache.kafka.clients.producer.ProducerRecord
@@ -11,20 +10,23 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
+private val log = KotlinLogging.logger {}
+
 /**
  * Bruker [suspendCoroutine] for å gjøre om send med [Callback] til en suspended function.
  */
-suspend inline fun <reified K : Any, reified V : Any> Producer<K, V>.sendAsync(
+suspend fun <K : Any, V : Any> Producer<K, V>.sendAsync(
     record: ProducerRecord<K, V>,
-): RecordMetadata = withContext(Dispatchers.IO) {
-    suspendCancellableCoroutine { continuation ->
-        val callback = Callback { metadata, exception ->
-            if (exception == null) {
-                continuation.resume(metadata)
-            } else {
-                continuation.resumeWithException(exception)
-            }
-        }
-        send(record, callback)
+): RecordMetadata = suspendCancellableCoroutine { continuation ->
+    continuation.invokeOnCancellation {
+        log.trace { "sendAsync cancelled, but message may still be delivered" }
     }
+    val callback = Callback { metadata: RecordMetadata, exception: Exception? ->
+        if (exception == null) {
+            continuation.resume(metadata)
+        } else {
+            continuation.resumeWithException(exception)
+        }
+    }
+    send(record, callback)
 }
