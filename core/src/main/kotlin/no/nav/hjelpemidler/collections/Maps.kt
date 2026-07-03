69588aeafa19b43840f328cb091e522beb272dfb
Erik Eiesland <erik.eiesland@nav.no>
2026-07-03 12:55:57 +0200

    fix: fjerner cast i Map.filterNotNull()

--- core/src/main/kotlin/no/nav/hjelpemidler/collections/Maps.kt
+++ core/src/main/kotlin/no/nav/hjelpemidler/collections/Maps.kt
@@ -5,9 +5,8 @@ import java.util.EnumMap
 inline fun <reified K : Enum<K>, V> enumMapOf(vararg pairs: Pair<K, V>): Map<K, V> =
     EnumMap<K, V>(K::class.java).apply { putAll(pairs) }
 
-@Suppress("UNCHECKED_CAST")
 fun <K, V> Map<K, V?>.filterNotNull(): Map<K, V> =
-    filterNot { it.value == null } as Map<K, V>
+    mapNotNull { (key, value) -> value?.let { key to it } }.toMap()
 
 fun <K : Any, V : Any> mapOfNotNull(pair: Pair<K, V?>): Map<K, V> {
     val (key, value) = pair
@@ -15,7 +14,7 @@ fun <K : Any, V : Any> mapOfNotNull(pair: Pair<K, V?>): Map<K, V> {
 }
 
 fun <K : Any, V : Any> mapOfNotNull(vararg pairs: Pair<K, V?>): Map<K, V> =
-    pairs.mapNotNull { (key, value) -> if (value == null) null else key to value }.toMap()
+    pairs.mapNotNull { (key, value) -> value?.let { key to it } }.toMap()
 
 fun Map<String, Any?>.joinToString(
     separator: CharSequence = ", ",
