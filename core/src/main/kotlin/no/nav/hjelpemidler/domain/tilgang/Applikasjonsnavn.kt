b32f461da48c3d8797dd173e217a5dda5d335e5c
Erik Eiesland <erik.eiesland@nav.no>
2026-07-21 12:14:05 +0200

    feat: Applikasjonsnavn.GJELDENDE

--- core/src/main/kotlin/no/nav/hjelpemidler/domain/tilgang/Applikasjonsnavn.kt
+++ core/src/main/kotlin/no/nav/hjelpemidler/domain/tilgang/Applikasjonsnavn.kt
@@ -1,6 +1,12 @@
 package no.nav.hjelpemidler.domain.tilgang
 
+import no.nav.hjelpemidler.configuration.NaisEnvironmentVariable
+
 /**
  * e.g. "hm-oppgave-sink"
  */
-class Applikasjonsnavn(value: String) : UtførtAvId(value)
+class Applikasjonsnavn(value: String) : UtførtAvId(value) {
+    companion object {
+        val GJELDENDE: Applikasjonsnavn by lazy { Applikasjonsnavn(NaisEnvironmentVariable.NAIS_APP_NAME) }
+    }
+}
