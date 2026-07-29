d61f0a81cebaf348fa2b94b13fc3aa8599f3d355
Sandra Elise Eng <117898702+Zandra7@users.noreply.github.com>
2026-07-29 14:13:51 +0200

    Legger til felter for epost og om oppfølgingsansvarlig er gjort oppmerksom (#10)

--- behovsmelding/src/main/kotlin/no/nav/hjelpemidler/behovsmeldingsmodell/v2/Innsenderbehovsmelding.kt
+++ behovsmelding/src/main/kotlin/no/nav/hjelpemidler/behovsmeldingsmodell/v2/Innsenderbehovsmelding.kt
@@ -162,6 +162,8 @@ data class Levering(
         val stilling: String,
         val telefon: String,
         val ansvarFor: String,
+        val epost: String? = null,
+        val erGjortOppmerksomPåOpplæringsansvar: Boolean? = null,
     ) : HarPersonnavn
 
     data class AnnenKontaktperson(
